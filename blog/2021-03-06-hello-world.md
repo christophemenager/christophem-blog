9357769fdb70ce3df1e05c3c80aa026bd7e3fa20
Christophe Menager <christophe.menager@matera.eu>
2021-03-06 13:15:58 +0100

    :tada: (first blog commit)

---
+++ blog/2021-03-06-hello-world.md
@@ -0,0 +1,16 @@
+---
+slug: hello-world
+title: Hello
+author: Chrichri
+author_url: https://github.com/christophemenager
+author_image_url: https://avatars.githubusercontent.com/u/17217868?s=400&u=4c0ead74e1a561ab95a75153619f14637aab91a8&v=4
+tags: [hello]
+---
+
+Welcome to my blog. This blog is created with [**Docusaurus 2 alpha**](https://v2.docusaurus.io/).
+
+<!--truncate-->
+
+This is a test post.
+
+A whole bunch of other information.
---
+++ docusaurus.config.js
@@ -0,0 +1,46 @@
+module.exports = {
+  title: 'Chrichri blog',
+  tagline: 'Welcom to my personal blog',
+  url: 'https://your-docusaurus-test-site.com',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'christophemenager', 
+  projectName: 'chrichri-nlog', 
+  themeConfig: {
+    navbar: {
+      title: "Chrichri's blog",
+      logo: {
+        alt: 'My Site Logo',
+        src: 'img/logo.svg',
+      },
+      items: [
+        {
+          href: 'https://github.com/christophemenager',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'light',
+      copyright: `Copyright © ${new Date().getFullYear()} Christophe Menager`,
+    },
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: false,
+        blog: {
+          path: './blog',
+          routeBasePath: '/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+};
